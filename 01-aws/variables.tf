2708d7d21bfc40f31894f1ca08a01d3e805beab5
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2023-09-08 19:32:32 +0100

    demo #2

---
+++ 01-aws/variables.tf
@@ -0,0 +1,38 @@
+variable "vpc_cidr" {
+  description = "Cidr block for vpc"
+  default     = "192.168.1.0/24"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "Cidr block for private subnet"
+  default     = "192.168.1.0/25"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "Cidr block for public subnet"
+  default     = "192.168.1.128/25"
+  type        = string
+}
+
+variable "ip_address_list" {
+  description = "A list that contains ip addresses"
+  type        = list(string)
+  default     = ["196.203.181.122/32"]
+}
+
+variable "instance_type" {
+  description = "The type of ec2 instance"
+  type        = string
+  default     = "t2.micro"
+
+}
+
+
+variable "rsa_public_key" {
+
+  description = "Rsa public key for ssh"
+  type        = string
+
+}
